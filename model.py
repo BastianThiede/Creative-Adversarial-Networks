963ee5fe20468e8cb6f64b19feafa87fba49c429
d4tah0g <k4ff33p0tt@gmail.com>
2019-04-25 15:56:41 +0200

    Removed debug prints

--- model.py
+++ model.py
@@ -310,16 +310,13 @@ class DCGAN(object):
           self.writer.add_summary(summary_str,counter)
         #Update G: don't need labels or inputs
           merged = tf.summary.merge_all()
-          _, summary_str,detail = self.sess.run([self.g_update, self.sums[1], merged],
+          _, summary_str = self.sess.run([self.g_update, self.sums[1]],
             feed_dict={
-              self.inputs: batch_images,
               self.z: batch_z,
-              self.y: batch_labels,
             })
 
 
           self.writer.add_summary(summary_str, counter)
-          self.writer.add_summary(detail, counter)
           #do we need self.y for these two?
           errD_fake = self.d_loss_fake.eval({
               self.z: batch_z,
