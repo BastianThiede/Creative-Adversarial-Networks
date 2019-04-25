a94c449c97b9c7f27deaaff74530e32fd4db6552
d4tah0g <k4ff33p0tt@gmail.com>
2019-04-25 16:53:20 +0200

    And now replacing zeros properly

--- losses.py
+++ losses.py
@@ -29,15 +29,15 @@ def CAN_loss(model):
     false_label = tf.random_uniform(tf.shape(model.D_), 0.0, 0.3)
 
     model.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(tf.add(model.D_, tf.constant(1e-15)) ,
+      sigmoid_cross_entropy_with_logits(tf.where(tf.equal(model.D_,0),1e-10 * tf.ones_like(model.D_),model.D_),
                                         true_label * tf.ones_like(model.D)))
 
     model.d_loss_fake = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(tf.add(model.D_logits_, tf.constant(1e-15)),
+      sigmoid_cross_entropy_with_logits(tf.where(tf.equal(model.D_logits_,0),1e-10,model.D_logits_),
                                         false_label * tf.ones_like(model.D_)))
 
     model.d_loss_class_real = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(logits=tf.add(model.D_c_logits, tf.constant(1e-15)),
+      tf.nn.softmax_cross_entropy_with_logits(logits=tf.where(tf.equal(model.D_c_logits,0),1e-10 * tf.ones_like(model.D_c_logits),model.D_c_logits),
                                               labels=model.smoothing * model.y))
 
     # if classifier is set, then use the classifier, o/w use the clasification layers in the discriminator
@@ -48,10 +48,10 @@ def CAN_loss(model):
     else:
       model.classifier = model.make_style_net(model.G)
       model.g_loss_class_fake = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=tf.add(model.classifier, tf.constant(1e-15)),
+        tf.nn.softmax_cross_entropy_with_logits(logits=tf.where(tf.equal(model.classifier,0),1e-10 * tf.ones_like(model.classifier),model.classifier),
       labels=(1.0/model.y_dim)*tf.ones_like(model.D_c_)))
 
-    model.g_loss_fake = -tf.reduce_mean(tf.log(tf.add(model.D_, tf.constant(1e-15))))
+    model.g_loss_fake = -tf.reduce_mean(tf.log(tf.where(tf.equal(model.D_,0),1e-10 * tf.ones_like(model.D_), model.D_)))
 
     model.d_loss = model.d_loss_real + model.d_loss_class_real + model.d_loss_fake
     model.g_loss = model.g_loss_fake + model.lamb * model.g_loss_class_fake
