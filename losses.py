49b1d109cb3330bf3f386e74057a1bd1fda94c81
d4tah0g <k4ff33p0tt@gmail.com>
2019-04-29 03:32:07 +0200

    Replacement value investigation

--- losses.py
+++ losses.py
@@ -2,8 +2,7 @@ import tensorflow as tf
 from ops import *
 
 def clip_tensor(tens):
-    return tens
-    mask = tf.equal(tens, 0.00001 * tf.ones_like(tens))
+    mask = tf.equal(tens, 1e-10 * tf.ones_like(tens))
     new_tensor = tf.multiply(tens, tf.cast(mask, 'float32'))
     return new_tensor
     #return tf.clip_by_value(tens,1e-10,1)
