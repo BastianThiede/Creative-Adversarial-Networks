2835cca5a0a9001dcb2a374cdd6bbb3b52ecf445
Bastian Thiede <k4FF33p0tt@gmail.com>
2019-06-02 18:27:05 +0200

    Vast changes of loss function

--- losses.py
+++ losses.py
@@ -37,32 +37,22 @@ def CAN_loss(model):
     true_label = tf.random_uniform(tf.shape(model.D),.8, 1.2)
     false_label = tf.random_uniform(tf.shape(model.D_), 0.05, 0.35)
 
-    model.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(clip_tensor(model.D_logits),
-                                        true_label * tf.ones_like(model.D)))
+    model.d_loss_real = - tf.reduce_mean(tf.log(model.D))
 
-    model.d_loss_fake = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(clip_tensor(model.D_logits_),
-                                        false_label * tf.ones_like(model.D_)))
+    model.d_loss_class_real = - tf.reduce_mean(
+      tf.nn.softmax_cross_entropy_with_logits(logits=model.D_c_logits,
+                                              labels=model.y))
 
-    model.d_loss_class_real = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(logits=clip_tensor(model.D_c_logits),
-                                              labels=model.smoothing * model.y))
-
-    # if classifier is set, then use the classifier, o/w use the clasification layers in the discriminator
-    if model.style_net_checkpoint is None:
-      model.g_loss_class_fake = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=clip_tensor(model.D_c_logits_),
-          labels=(1.0/model.y_dim)*tf.ones_like(model.D_c_)))
-    else:
-      model.classifier = model.make_style_net(model.G)
-      model.g_loss_class_fake = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=clip_tensor(model.classifier),
-      labels=(1.0/model.y_dim)*tf.ones_like(model.D_c_)))
-
-    model.g_loss_fake = -tf.reduce_mean(tf.log(model.D_))
+    model.d_loss_fake = - tf.reduce_mean(tf.log(1 - model.D_))
 
     model.d_loss = model.d_loss_real + model.d_loss_class_real + model.d_loss_fake
+    # if classifier is set, then use the classifier, o/w use the clasification layers in the discriminator
+    model.g_loss_class_fake = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=model.D_c_logits_,
+                                                labels=(1.0/model.y_dim) * model.D_c_))
+
+    model.g_loss_fake = -tf.reduce_mean(tf.log( 1 - model.D_))
+
     model.g_loss = model.g_loss_fake + model.lamb * model.g_loss_class_fake
 
     model.d_loss_real_sum       = scalar_summary("d_loss_real", model.d_loss_real)
