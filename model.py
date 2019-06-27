5068c654801cbf5051bf02737edb7771e5e881a4
Bastian Thiede <k4FF33p0tt@gmail.com>
2019-06-27 11:02:22 +0200

    Multiply Generator Training time

--- model.py
+++ model.py
@@ -329,7 +329,7 @@ class DCGAN(object):
           merged = tf.summary.merge_all()
           #Generator has a lot of trouble catching up to discriminator
           # Training G multiple times may reduce the gap
-          for i in range(3):
+          for i in range(2):
             _, summary_str  = self.sess.run([self.g_update, self.sums[1]],
               feed_dict={
                 self.inputs: batch_images,
