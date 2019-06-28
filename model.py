7c0734e2732d915bde1458ec44e0a7125f4f39ec
d4tah0g <k4ff33p0tt@gmail.com>
2019-06-28 23:43:08 +0200

    No debug calcs

--- model.py
+++ model.py
@@ -354,7 +354,7 @@ class DCGAN(object):
             self.y: batch_labels
           })
           train_time = time.time() - train_time_start
-          if True:
+          if False:
             #do we need self.y for these two?
             errD_tot = self.d_loss_total.eval({
                 self.inputs: batch_images,
@@ -480,9 +480,9 @@ class DCGAN(object):
           print("Epoch: [%2d] [%4d/%4d] time: %4.4f file_load: %4.4f train_time: %4.4f" \
             % (epoch, idx, batch_idxs,
               time.time() - start_time,file_load_time,train_time))
-          print("Discriminator class acc: %.2f" % (accuracy))
-          print("Err-d_class_real: {} Err-D_total: {},"
-                "Err-G_class_fake: {}".format(errD_class_real,errD_tot,errG_class_fake))
+          #print("Discriminator class acc: %.2f" % (accuracy))
+          #print("Err-d_class_real: {} Err-D_total: {},"
+          #      "Err-G_class_fake: {}".format(errD_class_real,errD_tot,errG_class_fake))
         else:
           if self.wgan:
             print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
